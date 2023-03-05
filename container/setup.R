8a26feab3f7bbbfa81bc08f065c81351be6d36de
Cory Latschkowski <koree@redhat.com>
2023-03-04 22:05:26 -0600

    fix: build

--- container/setup.R
+++ container/setup.R
@@ -1,17 +1,18 @@
 # https://github.com/r-lib/devtools/issues/2084
 # INSTALL_opts=c("--no-help", "--no-html")
+# install.packages('fs', repos = 'https://cran.microsoft.com/')
 
-install.packages('fs', repos = 'https://cran.microsoft.com/')
-install.packages('withr', repos = 'https://cran.microsoft.com/', INSTALL_opts=c("--no-help", "--no-html"))
+install.packages('fs')
+install.packages('withr', INSTALL_opts=c("--no-help", "--no-html"))
 
 library(withr)
 
 with_makevars(c(PKG_CFLAGS = "-std=c11"),
-    install.packages('shiny', repos = 'https://cran.microsoft.com/', INSTALL_opts=c("--no-help", "--no-html")),
+    install.packages('shiny', INSTALL_opts=c("--no-help", "--no-html")),
     assignment = "+=")
 
 with_makevars(c(PKG_CFLAGS = "-std=c11"),
-    install.packages('remotes', repos = 'https://cran.microsoft.com/', INSTALL_opts=c("--no-help", "--no-html")),
+    install.packages('remotes', INSTALL_opts=c("--no-help", "--no-html")),
     assignment = "+=")
 
 with_makevars(c(PKG_CFLAGS = "-std=c11"),
